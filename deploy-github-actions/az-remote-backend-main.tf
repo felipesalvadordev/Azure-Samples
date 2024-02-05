b5492cf5204c6855009eb5c9a46dcb470c250392
Felipe Salvador <f.salvador91@gmail.com>
2024-02-04 21:05:48 -0300

    Update az-remote-backend-main.tf

--- deploy-github-actions/az-remote-backend-main.tf
+++ deploy-github-actions/az-remote-backend-main.tf
@@ -1,3 +1,24 @@
+# Define Terraform provider
+terraform {
+  required_version = ">= 1.3"
+  backend "azurerm" {
+    resource_group_name  = "kopicloud-tfstate-rg"
+    storage_account_name = "kopicloudiactest"
+    container_name       = "core-tfstate"
+    key                  = "actions.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      version = "~>3.2"
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+# Configure the Azure provider
+provider "azurerm" { 
+  features {}  
+}
+
 # Generate a random storage name
 resource "random_string" "tf-name" {
   length = 8
